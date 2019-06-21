3d64baeb76bd67aca6c139ce6a0c4f45f833c32b
alexander <alexander.i.kamenev@gmail.com>
2019-06-21 09:10:08 +0300

    move on

--- converter.py
+++ converter.py
@@ -11,3 +11,21 @@ def divide_by2(dec_number: int):
 
     return ''.join(map(str, remstack[::-1]))
 
+
+digits = '0123456789ABCDEF'
+
+
+def divide_v2(dec_number: int, base: int) -> str:
+    """
+    >>> divide_v2(3, 2)
+    '11'
+    >>> divide_v2(10, 16)
+    'A'
+    """
+    if base > dec_number:
+        return digits[dec_number]
+    return f'{divide_v2(dec_number // base, base)}{digits[dec_number % base]}'
+
+
+def hanoy(depth, from_, to, with_):
+    pass
